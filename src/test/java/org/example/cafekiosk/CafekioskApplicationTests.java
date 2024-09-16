6a50860eddf6432c734200e5202a89db9759cd93
minh317 <minh317@github.com>
2024-09-16 16:02:14 +0900

    [feat] project initialize

---
+++ src/test/java/org/example/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
